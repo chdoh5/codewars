ba5d6ccbd71513da5b922d3cd2ba216c802137cd
github username <github email address>
2015-06-05 20:00:00 -0600

    new

--- index.js
+++ index.js
@@ -275,3 +275,29 @@
 // console.log(addBinary(12,1))
 
 // -----------
+// In this little assignment you are given a 
+// string of space separated numbers, and have to
+//  return the highest and lowest number.
+
+// Example:
+
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+// const highAndLow = (numbers) => {
+//     let arr = numbers.split(" ")
+//     let numArr = []
+
+//     arr.map( num => {
+//       numArr.push(parseInt(num))
+//     })
+
+//     let min = Math.min(...numArr).toString()
+//     let max = Math.max(...numArr).toString()
+//     return max + " " + min
+// }
+
+// console.log(highAndLow("1 2 3 4 5")) 
+
+kfkfk
