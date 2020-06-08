acadbd1c4dea21d7fb353494569f8b0ed4e2e26b
github username <github email address>
2020-06-08 16:43:03 -0500

    6/8/20

---
+++ console.rb
@@ -0,0 +1,38 @@
+# def even_or_odd(number)
+#     if number.even?
+#         print "Even"
+#     else 
+#         print "Odd"
+#     end
+# end
+
+# puts even_or_odd(8)
+
+# def positive_sum(arr)
+#     pos = arr.select{|n| n>0}
+#     p pos.sum
+# end
+
+# arr = [1, -4, 7, 12]
+# positive_sum(arr)
+
+# def find_uniq(arr)
+#   arr.each{ |n|
+#     if arr.count(n)===1
+#         puts n 
+#     end
+#     }
+# end
+
+# def find_uniq(arr)
+#     arr.each{|i|
+#         if arr.count(i)===1
+#             puts i
+#         end
+#     }
+
+# end
+
+# ar = [1,1,1,1,0]
+
+# p find_uniq(ar)
