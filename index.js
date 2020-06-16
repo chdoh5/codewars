280fc9adcc7a2a8baddcd41c77ed4da9eac66a72
github username <github email address>
2020-06-16 14:41:47 -0500

    js

--- index.js
+++ index.js
@@ -258,3 +258,20 @@
 // }
 
 // toCamelCase(strDash)
+
+// --------------
+
+// Implement a function that adds two numbers together and returns their sum in binary. 
+// The conversion can be done before, or after the addition.
+
+// The binary number returned should be a string.
+
+// const addBinary = (a,b) => {
+//     let sum = a + b
+
+//     return sum.toString(2)
+// }
+
+// console.log(addBinary(12,1))
+
+// -----------
